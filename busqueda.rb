ca123a27f34a9510fc2e429839d05fbfa3f8e1aa
Pablo <pquezada2009@hotmail.es>
2019-06-08 21:14:58 -0400

    desafio_hashes

---
+++ busqueda.rb
@@ -0,0 +1,23 @@
+ventas = {
+Enero: 15000,
+Febrero: 22000,
+Marzo: 12000,
+Abril: 17000,
+Mayo: 81000,
+Junio: 13000,
+Julio: 21000,
+Agosto: 41200,
+Septiembre: 25000,
+Octubre: 21500,
+Noviembre: 91000,
+Diciembre: 21000
+}
+
+def filter(data)
+    filter = {"Enero"=>15000, "no encontrado"=>31000, 
+    "no encontrado"=>27000}
+     end
+    return(filter)
+end
+
+puts filter
